7723ac7a883401a5ddb741f695d583b01aa117db
Túlio Rezende <tulio.rezende@outlook.com>
2026-02-17 17:58:35 -0300

    Add CORS rules to Azure Storage Account configuration
    Added CORS rules to blob properties for Azure Storage Account.

--- modules/azure-storage-account-medias/main.tf
+++ modules/azure-storage-account-medias/main.tf
@@ -4,6 +4,24 @@ resource "azurerm_storage_account" "grupo118finalchallengeinfrasa" {
   location                 = var.location
   account_replication_type = "LRS"
   account_tier             = "Standard"
+
+  blob_properties {
+    cors_rule {
+      allowed_origins    = ["http://localhost:3000"]
+      allowed_methods    = ["DELETE", "GET", "HEAD", "OPTIONS", "POST", "PUT"]
+      allowed_headers    = ["*"]
+      exposed_headers    = ["*"]
+      max_age_in_seconds = 3600
+    }
+
+    cors_rule {
+      allowed_origins    = ["*"]
+      allowed_methods    = ["DELETE", "GET", "HEAD", "OPTIONS", "POST", "PUT"]
+      allowed_headers    = ["*"]
+      exposed_headers    = ["*"]
+      max_age_in_seconds = 3600
+    }
+  }
 }
 
 resource "azurerm_storage_container" "grupo118finalchallengeinfrasacontainer" {
